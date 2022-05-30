e377e74b3df42ae16bc8a64c8b801a3e29541a05
Arshak Aghakaryan <agakaryan.arshak@gmail.com>
2022-05-30 20:37:08 +0400

    chore(interface): Export `TopBlockedBouncingScrollPhysics`

--- lib/flutter_top_blocked_bouncing_scroll_physics.dart
+++ lib/flutter_top_blocked_bouncing_scroll_physics.dart
@@ -1,7 +1,3 @@
 library flutter_top_blocked_bouncing_scroll_physics;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/top_blocked_bouncing_scroll_physics.dart';
